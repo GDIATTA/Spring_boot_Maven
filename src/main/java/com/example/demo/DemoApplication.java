d9bed6599c50d28332f1cd68b92cdba7fead8d84
GDIATTA <gaussjatta1@outlook.fr>
2024-03-10 13:47:09 +0100

    added some files

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+import com.example.demo.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+		                "Gauss",
+		                "gaussjatta1@gmail.com",
+		                 LocalDate.of(2000, Month.MARCH, 5),
+		                 30
+	                       )
+		        );
+	}
+}
